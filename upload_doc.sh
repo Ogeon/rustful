d428ccf8b3e541e5be74032f176cbf985caf0080
Erik Hedvall <erikwhedvall@gmail.com>
2014-12-27 21:31:39 +0100

    Include rustful_macros in docs, using some regex magic

--- upload_doc.sh
+++ upload_doc.sh
@@ -1,6 +1,10 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ -e doc ]; then
 echo Starting gh-pages upload...
 
+# Edit docs to only keep rustful related documents
+grep -v "'rustful" target/doc/search-index.js | sed -ne "s/^searchIndex\['\([a-z_\-]*\).*/doc\/\1 doc\/src\/\1/p" | xargs rm -r
+sed -ni "/'rustful\|^var\|^init/p" doc/search-index.js
+
 cp -r doc $HOME/doc
 
 # Go to home and setup git
