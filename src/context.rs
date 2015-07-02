03f4ab69b7fcf3307558904fc3ec69d669b663a0
Erik Hedvall <erikwhedvall@gmail.com>
2015-07-02 16:23:58 +0200

    Add examples and information about accessing some context data and utilities

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,136 @@
 //!Handler context and request body reading extensions.
+//!
+//!#Context
+//!
+//!The [`Context`][context] contains all the input data for the request
+//!handlers, as well as some utilities. This is where request data, like
+//!headers, client address and the request body can be retrieved from and it
+//!can safely be picked apart, since its ownership is transferred to the
+//!handler.
+//!
+//!##Accessing Headers
+//!
+//!The headers are stored in the `headers` field. See the [`Headers`][headers]
+//!struct for more information about how to access them.
+//!
+//!```
+//!use rustful::{Context, Response};
+//!use rustful::header::UserAgent;
+//!
+//!fn my_handler(context: Context, response: Response) {
+//!    if let Some(&UserAgent(ref user_agent)) = context.headers.get() {
+//!        let res = format!("got user agent string \"{}\"", user_agent);
+//!        response.into_writer().send(res);
+//!    } else {
+//!        response.into_writer().send("no user agent string provided");
+//!    }
+//!}
+//!```
+//!
+//!##Path Variables
+//!
+//!A router may collect variable data from paths (for example `id` in
+//!`/products/:id`). The values from these variables can be accessed through
+//!the `variables` field.
+//!
+//!```
+//!use rustful::{Context, Response};
+//!
+//!fn my_handler(context: Context, response: Response) {
+//!    if let Some(id) = context.variables.get("id") {
+//!        let res = format!("asking for product with id \"{}\"", id);
+//!        response.into_writer().send(res);
+//!    } else {
+//!        //This will usually not happen, unless the handler is also
+//!        //assigned to a path without the `id` variable
+//!        response.into_writer().send("no id provided");
+//!    }
+//!}
+//!```
+//!
+//!##Other URL Parts
+//!
+//! * Query variables (`http://example.com?a=b&c=d`) can be found in the
+//!`query` field and they are accessed in exactly the same fashion as path
+//!variables are used.
+//!
+//! * The fragment (`http://example.com#foo`) is also parsed and can be
+//!accessed through `fragment` as an optional `String`.
+//!
+//!##Logging
+//!
+//!Rustful has a built in logging infrastructure and it is made available to
+//!handlers through the `log` field. This provides logging and error reporting
+//!in a unified and more controlled fashion than what panics and `println!`
+//!gives. See the [`log`][log] module for more information about the standard
+//!alternatives.
+//!
+//!```
+//!# fn something_that_may_fail() -> Result<&'static str, &'static str> { Ok("yo") }
+//!use rustful::{Context, Response};
+//!use rustful::StatusCode::InternalServerError;
+//!
+//!fn my_handler(context: Context, mut response: Response) {
+//!    match something_that_may_fail() {
+//!        Ok(res) => response.into_writer().send(res),
+//!        Err(e) => {
+//!            context.log.error(&format!("it failed! {}", e));
+//!            response.set_status(InternalServerError);
+//!        }
+//!    }
+//!}
+//!```
+//!
+//!##Global Data
+//!
+//!There is also infrastructure for globally accessible data, that can be
+//!accessed through the `global` field. This is meant to provide a place for
+//!things like database connections or cached data that should be available to
+//!all handlers. The storage space itself is immutable when the server has
+//!started, so the only way to change it is through some kind of inner
+//!mutability.
+//!
+//!```
+//!use rustful::{Context, Response};
+//!use rustful::StatusCode::InternalServerError;
+//!
+//!fn my_handler(context: Context, mut response: Response) {
+//!    if let Some(some_wise_words) = context.global.get::<&str>() {
+//!        let res = format!("food for thought: {}", some_wise_words);
+//!        response.into_writer().send(res);
+//!    } else {
+//!        context.log.error("there should be a string literal in `global`");
+//!        response.set_status(InternalServerError);
+//!    }
+//!}
+//!```
+//!
+//!##Request Body
+//!
+//!The body will not be read in advance, unlike the other parts of the
+//!request. It is instead available as a `BodyReader` in the field `body`,
+//!through which it can be read and parsed as various data formats, like JSON
+//!and query strings. The documentation for [`BodyReader`][body_reader] gives
+//!more examples.
+//!
+//!```
+//!use std::io::{BufReader, BufRead};
+//!use rustful::{Context, Response};
+//!
+//!fn my_handler(context: Context, response: Response) {
+//!    let mut numbered_lines = BufReader::new(context.body).lines().enumerate();
+//!    let mut writer = response.into_writer();
+//!
+//!    while let Some((line_no, Ok(line))) = numbered_lines.next() {
+//!        writer.send(format!("{}: {}", line_no + 1, line));
+//!    }
+//!}
+//!```
+//!
+//![context]: struct.Context.html
+//![headers]: ../header/struct.Headers.html
+//![log]: ../log/index.html
+//![body_reader]: struct.BodyReader.html
 
 use std::collections::HashMap;
 use std::io::{self, Read};
