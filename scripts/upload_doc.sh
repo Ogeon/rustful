e7f2f1ac4ecfb070507d7b058f09caeabde0d173
Erik Hedvall <erikwhedvall@gmail.com>
2015-09-18 01:04:32 +0200

    Only upload documentation for Linux

--- scripts/upload_doc.sh
+++ scripts/upload_doc.sh
@@ -1,3 +1,3 @@
-if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
+if [ "$TRAVIS_RUST_VERSION" = "stable" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	curl https://raw.githubusercontent.com/ogeon/travis-doc-upload/master/travis-doc-upload.sh | sh
 fi
